0d4aa0dd1c61a139a741009cfe2d55aca283de47
Ryan <hiRyan@protonmail.com>
2018-11-29 20:32:35 +0200

    Viewing a contact from the database with Thymeleaf

--- src/main/java/ee/sdacademy/thymleaf/contacts/controllers/IndexPageController.java
+++ src/main/java/ee/sdacademy/thymleaf/contacts/controllers/IndexPageController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import ee.sdacademy.thymleaf.contacts.services.ContactService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexPageController {
@@ -13,9 +16,28 @@ public class IndexPageController {
     @Autowired
     private ContactService contactService;
 
+    // all contacts
     @GetMapping("/")
     public String mainPage(Model model) {
         model.addAttribute("contacts", contactService.findAll());
         return "index";
     }
+
+    // an individual contact
+    @GetMapping("/view")
+    public String viewContact(@RequestParam("id") Integer id, Model model){
+        model.addAttribute("contact", contactService.get(id));
+        return "view";
+    }
+
+
+//    // an individual contact - an alternative methodology of above
+//    // MANUALLY put in the url: /view/1
+//    @GetMapping("/view/{id}")
+//    public String viewContactWithPath(@PathVariable("id") Integer id, Model model){
+//        model.addAttribute("contact", contactService.get(id));
+//        return "view";
+//    }
+
+
 }
